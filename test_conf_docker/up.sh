3caf0babdadc4bd024e08c8164d4a305bcd1ab49
tukka25 <aboodytukka@gmail.com>
2023-12-11 07:42:46 +0400

    any thing

--- test_conf_docker/up.sh
+++ test_conf_docker/up.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-echo -e "\033[0;36mPlease Wait To Builddd\033[0m"
-docker build -t $1 .
-docker run -d -p 443:443 --name $2 $1
-export deleteme=$1
-docker exec -it $2 /bin/bash
+sudo echo -e "\033[0;36mPlease Wait To Builddd\033[0m"
+sudo docker build -t "ngin" .
+sudo docker run -d -p 443:443 --name "ngin4" "ngin"
+sudo docker exec -it "ngin4" /bin/bash
